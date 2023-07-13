0e27140c01fe2feede9988e2ec553ff904a93de5
15jkw205 <brawlgodbs@gmail.com>
2023-07-13 12:44:56 -0600

    this is an update

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello world, but in git!")
+print("Hello world, but in git!\n")
+
+for i in range(0, 3, 1):
+    for j in range (0, 5, 1):
+        print('*', end="")
+
+    print("")
+
+    
+print("")
+print("This simple project prints a rectangle.")
+
+        
