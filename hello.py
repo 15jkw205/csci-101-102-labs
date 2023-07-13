bdb318f18132b49913038189cbee3221b34b8eba
15jkw205 <brawlgodbs@gmail.com>
2023-07-13 12:54:42 -0600

    this is another commit

--- hello.py
+++ hello.py
@@ -1,13 +1,1 @@
-print("Hello world, but in git!\n")
-
-for i in range(0, 3, 1):
-    for j in range (0, 5, 1):
-        print('*', end="")
-
-    print("")
-
-    
-print("")
-print("This simple project prints a rectangle.")
-
-        
+print("Hello world, but in git!")
